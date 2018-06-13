3e890dd5b71162a52c01d382aa981657cbe7ea1b
krokowski0x <krokowski.dx@gmail.com>
2018-06-13 22:56:43 +0200

    Added tests (exluding one) to Chapter 1

---
+++ test/Chapter1.test.js
@@ -0,0 +1,132 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+const dirname = '../src/Chapter 1 - Arrays and Strings/';
+
+const isUnique =              require(`${dirname}isUnique`);
+const checkPermutation =      require(`${dirname}checkPermutation`);
+const URLify =                require(`${dirname}URLify`);
+const palindromePermutation = require(`${dirname}palindromePermutation`);
+const oneAway =               require(`${dirname}oneAway`);
+const stringCompression =     require(`${dirname}stringCompression`);
+const rotateMatrix =          require(`${dirname}rotateMatrix`);
+const zeroMatrix =            require(`${dirname}zeroMatrix`);
+const stringRotation =        require(`${dirname}stringRotation`);
+
+describe('Chapter 1', () => {
+
+  describe('#isUnique()', () => {
+    it('should work for simple words', () => {
+      expect(isUnique('this')).to.be.true;
+      expect(isUnique('lel')).to.be.false;
+    });
+
+    it('should not include spaces', () => {
+      expect(isUnique(' t h i s ')).to.be.true;
+      expect(isUnique('this is false')).to.be.false;
+    });
+  });
+
+  describe('#checkPermutation()', () => {
+    it('should work for simple words', () => {
+      expect(checkPermutation('john', 'nohj')).to.be.true;
+      expect(checkPermutation('john', 'bob')).to.be.false;
+    });
+
+    it('should not include spaces', () => {
+      expect(checkPermutation('john', 'n  oh j  ')).to.be.true;
+    });
+  });
+
+  describe('#URLify()', () => {
+    it('should work for basic examples', () => {
+      expect(URLify('Mr John Smith')).to.equal('Mr%20John%20Smith');
+    });
+
+    it('should not include spaces at the end', () => {
+      expect(URLify('Mr John Smith        ')).to.equal('Mr%20John%20Smith');
+    });
+  });
+
+  describe('#palindromePermutation()', () => {
+    it('should work for simple words', () => {
+      expect(palindromePermutation('eye')).to.be.true;
+      expect(palindromePermutation('llel')).to.be.false;
+    });
+
+    it('should ignore spaces', () => {
+      expect(palindromePermutation('tact coa')).to.be.true;
+      expect(palindromePermutation('this won\'t work')).to.be.false;
+    });
+  });
+
+  describe('#oneAway()', () => {
+    it('should work for inserting a character', () => {
+      expect(oneAway('pale', 'pales')).to.be.true;
+      expect(oneAway('pale', 'palessss')).to.be.false;
+    });
+
+    it('should work for removing a character', () => {
+      expect(oneAway('pale', 'ple')).to.be.true;
+      expect(oneAway('pale', 'p')).to.be.false;
+    });
+
+    it('should work for replacing a character', () => {
+      expect(oneAway('pale', 'bale')).to.be.true;
+      expect(oneAway('pale', 'bake')).to.be.false;
+    });
+  });
+
+  describe('#stringCompression()', () => {
+    it('should return original string if it\'s shorter', () => {
+      expect(stringCompression('abcd')).to.equal('abcd');
+      expect(stringCompression('aabbccdd')).to.equal('aabbccdd');
+    });
+
+    it('should compress string when possible', () => {
+      expect(stringCompression('aabcccccaaa')).to.equal('a2b1c5a2');
+    });
+  });
+
+  describe('#rotateMatrix()', () => {
+    it('should rotate the matrix by 90 degrees', () => {
+      let input = [
+        [1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12],
+        [13,14,15,16]
+      ];
+      let output = [
+        [13,9,5,1],
+        [14,10,6,2],
+        [15,11,7,3],
+        [16,12,8,4]
+      ];
+      expect(rotateMatrix(input)).to.deep.equal(output);
+    })
+  });
+
+  // describe('#zeroMatrix()', () => {
+  //   it('should work', () => {
+  //     let input = [
+  //       [1,2,3,4,5],
+  //       [1,0,3,4,5],
+  //       [1,2,3,0,5],
+  //       [1,2,3,4,5]
+  //     ];
+  //     let output = [
+  //       [1,0,3,0,5],
+  //       [0,0,0,0,0],
+  //       [0,0,0,0,0],
+  //       [1,0,3,0,5]
+  //     ];
+  //     expect(zeroMatrix(input)).to.deep.equal(output);
+  //   })
+  // });
+
+  describe('#stringRotation()', () => {
+    it('should work', () => {
+      expect(stringRotation('waterbottle', 'erbottlewar')).to.be.true;
+    });
+  });
+});
