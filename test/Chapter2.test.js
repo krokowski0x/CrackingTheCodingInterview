54571552305dd8083f7cec3611e69992178548e2
krokowski0x <krokowski.dx@gmail.com>
2018-06-16 22:20:35 +0200

    Filled out all the tests to Chapter 2

--- test/Chapter2.test.js
+++ test/Chapter2.test.js
@@ -1,16 +1,16 @@
-const chai = require('chai');
+import { expect } from 'chai';
 
-const expect = chai.expect;
-const dirname = '../src/Chapter 2 - Linked Lists/';
+import { LinkedList, Node } from '../Data Structures/Chapter 2 - Linked Lists/LinkedList';
+import removeDups           from '../Data Structures/Chapter 2 - Linked Lists/removeDups';
+import kthToLast            from '../Data Structures/Chapter 2 - Linked Lists/kthToLast';
+import deleteMiddleNode     from '../Data Structures/Chapter 2 - Linked Lists/deleteMiddleNode';
+import partition            from '../Data Structures/Chapter 2 - Linked Lists/partition';
+import sumLists             from '../Data Structures/Chapter 2 - Linked Lists/sumLists';
+import palindrome           from '../Data Structures/Chapter 2 - Linked Lists/palindrome';
+import intersection         from '../Data Structures/Chapter 2 - Linked Lists/intersection';
+import loopDetection        from '../Data Structures/Chapter 2 - Linked Lists/loopDetection';
 
-const { LinkedList, Node } = require(`${dirname}LinkedList`);
-const removeDups =           require(`${dirname}removeDups`);
-const kthToLast =            require(`${dirname}kthToLast`);
-const deleteMiddleNode =     require(`${dirname}deleteMiddleNode`);
-const sumLists =             require(`${dirname}sumLists`);
-const palindrome =           require(`${dirname}palindrome`);
-
-describe('Chapter 2', () => {
+describe('Chapter 2 - Linked Lists', () => {
   describe('#removeDups()', () => {
     it('should remove duplicate nodes', () => {
       const inputList = new LinkedList();
@@ -18,6 +18,7 @@ describe('Chapter 2', () => {
 
       inputList.add(2).add(3).add(45).add(456).add(54).add(2).add(3).add(11);
       outputList.add(2).add(3).add(45).add(456).add(54).add(11);
+
       expect(removeDups(inputList).show()).to.equal(outputList.show());
     });
   });
@@ -27,6 +28,7 @@ describe('Chapter 2', () => {
       const inputList = new LinkedList();
 
       inputList.add(2).add(3).add(45).add(456).add(54).add(2).add(3).add(11);
+
       expect(kthToLast(inputList, 4)).to.equal(54);
     });
 
@@ -34,6 +36,7 @@ describe('Chapter 2', () => {
       const inputList = new LinkedList();
 
       inputList.add(2).add(3).add(45).add(456).add(54).add(2).add(3).add(11);
+
       expect(() => kthToLast(inputList, 20)).to.throw(Error);
     });
 
@@ -41,6 +44,7 @@ describe('Chapter 2', () => {
       const inputList = new LinkedList();
 
       inputList.add(2).add(3).add(45).add(456).add(54).add(2).add(3).add(11);
+
       expect(() => kthToLast(inputList, -1)).to.throw(Error);
     });
   });
@@ -53,6 +57,7 @@ describe('Chapter 2', () => {
 
       inputList.add(2).add(3).add(45).add(456).add(54).add(2).add(3).add(11);
       outputList.add(2).add(3).add(45).add(54).add(2).add(3).add(11);
+
       expect(deleteMiddleNode(inputList, node).show()).to.equal(outputList.show());
     });
 
@@ -61,12 +66,26 @@ describe('Chapter 2', () => {
       const outputList = new LinkedList();
 
       inputList.add(2).add(3).add(45).add(456).add(54).add(2).add(3).add(11);
+
       const head = inputList.head;
-      const tail = inputList.get(inputList.length-1);
+      const tail = inputList.get(inputList.length - 1);
+
       expect(() => deleteMiddleNode(inputList, head)).to.throw(Error);
       expect(() => deleteMiddleNode(inputList, tail)).to.throw(Error);
     });
   });
+
+  // describe('#partition()', () => {
+  //   it('should partition list around given node', () => {
+  //     const inputList = new LinkedList();
+  //     const outputList = new LinkedList();
+  //
+  //     inputList.add(3).add(5).add(8).add(5).add(10).add(2).add(1);
+  //     outputList.add(3).add(1).add(2).add(10).add(5).add(5).add(8);
+  //
+  //     expect(partition(inputList.head, 5)).to.deep.equal(outputList.head);
+  //   });
+  // });
 
   describe('#sumLists()', () => {
     it('should sum two lists', () => {
@@ -77,6 +96,7 @@ describe('Chapter 2', () => {
       inputList1.add(7).add(1).add(6);
       inputList2.add(5).add(9).add(2);
       outputList.add(2).add(1).add(9);
+
       expect(sumLists(inputList1, inputList2).show()).to.equal(outputList.show());
     });
   });
@@ -91,6 +111,56 @@ describe('Chapter 2', () => {
 
       expect(palindrome(inputList1)).to.be.true;
       expect(palindrome(inputList2)).to.be.false;
+    });
+  });
+
+  describe('#intersection()', () => {
+    it('should detect if two lists intersect', () => {
+      var a = new Node('a');
+      var b = new Node('b');
+      var c = new Node('c');
+      var d = new Node('d');
+      var e = new Node('e');
+      var f = new Node('f');
+      var g = new Node('g');
+      var h = new Node('h');
+
+      a.next = b;
+      b.next = c;
+      c.next = d;
+      d.next = e;
+      e.next = f;
+      f.next = g;
+      g.next = h;
+
+      var a1 = new Node('a1');
+      var b1 = new Node('b1');
+      var c1 = new Node('c1');
+
+      a1.next = b1;
+      b1.next = c1;
+      c1.next = d;
+
+      expect(intersection(a, a1)).to.be.true;
+    });
+  });
+
+  describe('#loopDetection()', () => {
+    it('should detect if list has a loop in it', () => {
+      const a = new Node();
+      const b = new Node();
+      const c = new Node();
+      const d = new Node();
+      const e = new Node();
+      const f = new Node();
+
+      a.next = b;
+      b.next = c;
+      c.next = d;
+      d.next = e;
+      e.next = f;
+      f.next = c;
+      expect(loopDetection(a)).to.be.true;
     });
   });
 });
